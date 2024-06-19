9defd3a83ad3d3d8c3104bc6b5f26d152851eafc
khoulaagzenay <149625297+khoulaagzenay@users.noreply.github.com>
2024-06-19 14:34:34 +0200

    Add program file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using constructeurs;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bienvenue dans le programme de gestion des bâtiments!");
+
+        Building BatA = new Building(40);
+
+        Console.WriteLine("Batiment A");
+        BatA.display();
+
+    }
+
+}
